94c72b10d466fc4fb548d8b1dbd2f80bdf09bd05
AbuAbdul1ah <musa_hekim@hotmail.co.uk>
2023-06-30 11:03:18 +0100

    Corrected indentation and disabled line too long (#2)

--- tests/test_sksurgeryspherefittingtest2_demo.py
+++ tests/test_sksurgeryspherefittingtest2_demo.py
@@ -2,13 +2,13 @@
 
 """scikit-surgery-sphere-fitting tests"""
 
-from sksurgeryspherefittingtest2.ui.sksurgeryspherefittingtest2_demo import run_demo
+from sksurgeryspherefittingtest2.ui.sksurgeryspherefittingtest2_demo import run_demo # pylint: disable=line-too-long
 
 def test_fit_sphere_least_sqs_demo():
-  """
-  test the run demo entry point
-  """
-  model_name = 'data/CT_Level_1.vtp'
-  output_name = 'out_temp.vtp'
+    """
+    test the run demo entry point
+    """
+    model_name = 'data/CT_Level_1.vtp'
+    output_name = 'out_temp.vtp'
 
-  run_demo(model_name, output_name)
+    run_demo(model_name, output_name)
